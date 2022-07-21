3f0ed8814e985e56e22897df98a2a651e70cc778
pandl <yjkimjunior@gmail.com>
2022-07-21 19:45:48 +0900

    feat: multiownable.sol and entitty[][]

--- src/SafariBang.sol
+++ src/SafariBang.sol
@@ -3,23 +3,49 @@ pragma solidity ^0.8.10;
 
 import "solmate/tokens/ERC721.sol";
 import "openzeppelin-contracts/contracts/utils/Strings.sol";
-import "openzeppelin-contracts/contracts/access/Ownable.sol";
+
+import "./MultiOwnable.sol";
 
 error MintPriceNotPaid();
 error MaxSupply();
 error NonExistentTokenUri();
 error WithdrawTransfer();
 
-contract SafariBang is ERC721, Ownable {
+contract SafariBang is ERC721, MultiOwnable {
     using Strings for uint256;
     string public baseURI;
     uint256 public currentTokenId = 0;
     uint256 public constant TOTAL_SUPPLY = 10_000;
     uint256 public constant MINT_PRICE = 0.08 ether;
 
+    enum EntittyType {
+        DOMESTICATED_ANIMAL,
+        WILD_ANIMAL,
+        EMPTY
+    }
+
+    struct Entitty {
+        EntittyType entittyType;
+        uint256 id;
+        uint256 size;
+        uint256 strength;
+        uint256 speed;
+        uint256 libido;
+        bool gender; // animals are male or female, no in-between ladyboy shit like in our stupid IRL world
+        uint32[][] position;
+        address owner;
+    }
+
+    Entitty[128][128] public safariMap;
+
     constructor(string memory _name, string memory _symbol, string memory _baseURI) ERC721(_name, _symbol) {
         baseURI = _baseURI;
+        transferSuperOwnership(msg.sender);
     }
+
+    function fight() public payable returns (bool) {}
+    function fuck() public payable returns (bool) {}
+    function flee() public payable returns (bool) {}
 
     function mintTo(address to) public payable returns (uint256) {
         if (msg.value < MINT_PRICE) {
@@ -45,7 +71,7 @@ contract SafariBang is ERC721, Ownable {
         return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, tokenId.toString())) : "";
     }
 
-    function withdrawPayments(address payable payee) external onlyOwner {
+    function withdrawPayments(address payable payee) external onlySuperOwner {
         uint256 balance = address(this).balance;
         (bool transferTx, ) = payee.call{value: balance}("");
         if (!transferTx) {
