bb8344b01ff209328f1d5edcc9269fc79df2850d
pandl <yjkimjunior@gmail.com>
2022-07-22 10:39:23 +0900

    fix: species enum

--- src/SafariBang.sol
+++ src/SafariBang.sol
@@ -18,14 +18,39 @@ contract SafariBang is ERC721, MultiOwnable {
     uint256 public constant TOTAL_SUPPLY = 10_000;
     uint256 public constant MINT_PRICE = 0.08 ether;
 
+    // _superOwner will always be the contract address, in order to clear state with asteroid later.
     enum EntittyType {
-        DOMESTICATED_ANIMAL,
-        WILD_ANIMAL,
+        DOMESTICATED_ANIMAL, // _owner is some Eth address
+        WILD_ANIMAL, // _owner is SafariBang contract address
         EMPTY
+    }
+
+    enum Species {
+        ZEBRAT, // Zebra with a bratty attitude
+        LIONNESSY, // Lionness thinks she's a princess
+        DOGGIE, // self explanatory canine slut
+        PUSSYCAT, // self explanatory slut but feline
+        THICCHIPPO, // fat chick
+        GAZELLA, // jumpy anxious female character
+        MOUSEY, // Spouse material
+        WOLVERINERASS, // wolf her in her ass i dunno man
+        ELEPHAT, // phat ass
+        RHINOCERHOE, // always horny
+        CHEETHA, // this cat ain't loyal
+        BUFFALO, // hench stud
+        MONKGOOSE, // zero libido just meditates
+        WARTHOG, // genital warts
+        BABOOB, // double D cup baboon
+        WILDEBEEST, // the other stud
+        IMPALA, // the inuendos just write themselves at this point
+        COCKODILE, // i may need professional help
+        HORNBILL, // who names these animals
+        OXPECKER // this bird is hung like an ox
     }
 
     struct Entitty {
         EntittyType entittyType;
+        Species species; // this determines the image
         uint256 id;
         uint256 size;
         uint256 strength;
